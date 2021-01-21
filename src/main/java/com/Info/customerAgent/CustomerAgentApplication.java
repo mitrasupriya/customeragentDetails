682bb1c4bfd18358c0d2d6418f80fea462e2e9dd
mitrasupriya <mitrasupriya3@gmail.com>
2021-01-22 00:11:11 +0530

    first commit

---
+++ src/main/java/com/Info/customerAgent/CustomerAgentApplication.java
@@ -0,0 +1,13 @@
+package com.Info.customerAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAgentApplication.class, args);
+	}
+
+}
